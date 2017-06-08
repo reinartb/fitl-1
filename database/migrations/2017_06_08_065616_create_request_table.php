4d314cce59dc383a587a67719079681df4b08a38
Reinart Bacalso <reinartbacalso@gmail.com>
2017-06-08 15:05:46 +0800

    added a request database table migration file

---
+++ database/migrations/2017_06_08_065616_create_request_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('requested_by_user');
+            $table->string('requested_by_section');
+            $table->string('ris_number');
+            $table->string('purpose')->nullable();           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+}
