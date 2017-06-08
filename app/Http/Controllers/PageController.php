984cf5f8530d32624340995ed7af6265f89e5851
Reinart Bacalso <reinartbacalso@gmail.com>
2017-06-08 14:43:37 +0800

    Initial laravel application.

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller {
+
+    public function about() {
+        return view('pages/about');
+    }
+
+
+}
+
+
+
+
+
+
+?>
